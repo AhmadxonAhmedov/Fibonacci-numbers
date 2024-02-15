6b0171b0165413b860cec0b0207b5ab9fc3585d1
Ahmadxon <ahmadxonahmedov64@gmail.com>
2024-02-15 21:53:22 +0500

    Add Commit

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
-﻿Console.WriteLine("Fibonacci raqamlarni aniqlovchi loyixa: ");
+﻿
 
+
+
+
+
+
+Console.WriteLine("Fibonacci raqamlarni aniqlovchi loyixa: ");
 Console.Write("Enter a number: ");
-            string userStrInput = Console.ReadLine();
-            int userInput = Convert.ToInt32(userStrInput);
-            int num1 = 0; int num2 = 1; int result = 0;
-            for(int i = 0; i < userInput; i++)
+    string userStrInput = Console.ReadLine();
+    int userInput = Convert.ToInt32(userStrInput);
+    int num1 = 0; int num2 = 1; int result = 0;
+    
+        for(int i = 0; i < userInput; i++)
+        {
+            result = num1 + num2; 
+            num1 = num2; 
+            num2 = result;
+            if(result <= userInput)
             {
-                result = num1 + num2; 
-                num1 = num2; 
-                num2 = result;
-                if(result <= userInput)
-                {
-                    Console.WriteLine(result);
-                }
+                Console.WriteLine(result);
             }
+         }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Fibonacci-numbers
-this project finds fibonacci numbers in C#
+
+
+# Fibonacci sonlarni aniqlab beruvchi dastur
+----
+***ushbu kichik loyixa orqali Fibonacci raqamlarni aniqlashingiz mumkin:***
+
+>Fibonacci sonlar quyidagicha tariflanadi: Avvalgi ikki elementni 1 ga teng bolib, 3 - elementdan boshlab ***xar bir element ozidan oldinga ikki yigindisiga teng*** qonuniyati asosida tuzilgan ketma-ketlikka Fibonacci ketma-ketligi, bu sonlarni esa, [Fibonacci sonlari deyiladi](https://uz.wikipedia.org/wiki/Fibonacci_sonlari)
+
+# Birinchi qism
+
+![alt text](image.png)
+
+# Ikkinchi qism
+
+![alt text](image-1.png)
